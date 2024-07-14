fa3c41fd22e2963413987343c42fa1250a986a7b
Simjae Holman-Rayos <simjaehr@gmail.com>
2024-07-14 15:09:47 -0400

    Added funtion that creates a file if not present or appends file if it is present

--- Password Program/main.cpp
+++ Password Program/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std; 
 
 
 class User
 {
 private:
-	string username, password; 
+	string username, password;
+	int accountNumber; 
 public: 
-	User(string user_name, string use_password)
+	User(string user_name, string use_password, int account_number)
 	{
 		username = user_name;
 		password = use_password;
+		accountNumber = account_number;
+		
 	}
 
 	~User()
@@ -32,10 +36,16 @@ public:
 	{
 		return password; 
 	}
+
+	int getAccountNumber()
+	{
+		return accountNumber; 
+	}
 	
 };
 
-User createUser()
+// Sets the user's name and password
+User createUser(int accountNumber)
 {
 	string name, password;
 
@@ -44,16 +54,27 @@ User createUser()
 	cout << "Password: ";
 	cin >> password; 
 
-	User* user = new User(name, password);
+	User* user = new User(name, password, accountNumber);
 
 	return *user;
 }
 
+// Creates a file name "Users" if one doesn't already exist and adds users to it
+void createFile(User user)
+{
+	fstream myFile;
+	myFile.open("Users", ios::app);
+	myFile <<  user.getUsername();
+	myFile << user.getPassword();
+	myFile << user.getAccountNumber();
+	myFile.close();
+}
+
 int main() 
 {
-	User user1 = createUser();
-	cout << user1.getUsername();
-	cout << user1.getPassword();
-
+	int numberOfAccounts = 0; 
+	numberOfAccounts += 1; 
+	User user1 = createUser(numberOfAccounts);
+	createFile(user1);
 	return 0; 
 }
