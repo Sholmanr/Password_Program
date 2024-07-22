bb3e389a3af0e36e2faf581a6729fcc40b279ade
Simjae Holman-Rayos <simjaehr@gmail.com>
2024-07-22 17:09:11 -0400

    Added a linked list and started the class to read from the file

--- Password Program/main.cpp
+++ Password Program/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std; 
 
+int numberOfAccounts = 0;
 
 class User
 {
@@ -23,6 +25,7 @@ public:
 		username = user_name;
 		password = use_password;
 		accountNumber = account_number;
+		
 		
 	}
 
@@ -53,7 +56,19 @@ public:
 	
 };
 
+class node
+{
+public:
+	User user;
+	node* next; 
 
+	node(User currentUser)
+	{
+		user = currentUser;
+		next = NULL;
+	}
+
+};
 
 // Sets the user's name and password
 User createUser(int accountNumber)
@@ -71,13 +86,21 @@ User createUser(int accountNumber)
 }
 
 // Creates a file name "Users" if one doesn't already exist and adds users to it
-void createFile(User user)
+void createFile(node *currentUser)
 {
 	fstream myFile;
-	myFile.open("Users", ios::app);
-	myFile <<  user.getUsername();
-	myFile << user.getPassword();
-	myFile << user.getAccountNumber();
+	myFile << numberOfAccounts;
+
+	while (currentUser->next != NULL)
+	{
+		myFile.open("Users", ios::app);
+		myFile <<  currentUser->user.getUsername();
+		myFile << "\n";
+		myFile << currentUser->user.getPassword();
+		myFile << "\n"; 
+		myFile << currentUser->user.getAccountNumber();
+		myFile << "\n";
+	}
 	myFile.close();
 }
 
@@ -86,28 +109,78 @@ bool infoCheck()
 	return true; 
 }
 
+// Checks if the username and password are correct
+bool checkPassword() 
+{
+	return true;
+}
+
+void addUserToList(node *&head)
+{
+	User user;
+	numberOfAccounts += 1;
+
+	user = createUser(numberOfAccounts);
+
+	node *n = new node(user);
+	if (head == NULL)
+	{
+		head = n; 
+	}
+	else
+	{
+		n->next = head;
+		head = n; 
+		
+	}
+}
+
+void getUsers()
+{
+	User user;
+	string name, password; 
+	int accountNumber; 
+
+	ifstream inputFile("Users.txt");
+
+	if (!inputFile.is_open())
+	{
+		return; 
+	}
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		getline(inputFile, name);
+		getline(inputFile, password);
+		inputFile >> accountNumber; 
+	}
+
+	node* n = new node(user);
+
+}
+
+
 int main() 
 {
-	int numberOfAccounts = 0, choice = 0;
+	int choice = 0;
+	node *head = NULL; 
 	bool proceed = true, valid = false;
 	char answer = ' ';
-	User user;  
 
 	while (proceed == true)
 	{
 		// Displays the choices and gets the choice from the user
 		cout << "What would you like to do?";
 		cout << "\n1. Create a new account";
-		cout << "\n2. Sign in"; 
+		cout << "\n2. Sign in\n"; 
 		cin >> choice; 
 		
 			// Determines what to do depending on what the user input 
 		switch (choice)
 		{
 		case 1:
-			numberOfAccounts += 1;
-			user = createUser(numberOfAccounts);
-			createFile(user);
+			addUserToList(head);
 			break;
 
 		case 2:
@@ -129,7 +202,7 @@ int main()
 				valid = true; 
 				break; 
 			}
-			else if (asnwer == 'n')
+			else if (answer == 'n')
 			{
 				proceed = false;
 				valid = true; 
@@ -149,6 +222,9 @@ int main()
 	}
 
 	cout << "Thank you for your transaction!"; 
+	createFile();
 	
 	return 0; 
 }
+
+// Need to make sure there are no special characters in the username 
